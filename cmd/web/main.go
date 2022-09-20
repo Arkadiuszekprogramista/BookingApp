50e34b88d75b67fc74b130c128b67bbc474a4c29
arkadiuszekprogramista <arkadiuszlaskowski1987@gmail.com>
2022-09-20 20:46:23 +0200

    adding better error handling

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -5,11 +5,13 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 	"time"
 
 	"github.com/alexedwards/scs/v2"
 	"github.com/arkadiuszekprogramista/bookingapp/internal/config"
 	"github.com/arkadiuszekprogramista/bookingapp/internal/handlers"
+	"github.com/arkadiuszekprogramista/bookingapp/internal/helpers"
 	"github.com/arkadiuszekprogramista/bookingapp/internal/models"
 	"github.com/arkadiuszekprogramista/bookingapp/internal/render"
 )
@@ -17,6 +19,8 @@ import (
 const portNumber = ":8080"
 var app config.AppConfig
 var session *scs.SessionManager
+var infoLog *log.Logger
+var errorLog *log.Logger
 
 
 // main is the main application function
@@ -44,6 +48,12 @@ func run() error {
 		// change this to true when in production
 		app.InProduction = false
 	
+		infoLog = log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+		app.InfoLog = infoLog
+
+		errorLog = log.New(os.Stdout, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+		app.ErrorLog = errorLog
+
 		// Set up the session
 		session = scs.New()
 		session.Lifetime = 24 * time.Hour
@@ -65,5 +75,7 @@ func run() error {
 		repo := handlers.NewRepo(&app)
 		handlers.NewHandlers(repo)
 		render.NewTemplates(&app)
+		helpers.NewHelpers(&app)
+
 	return nil
 }
---
+++ internal/helpers/helpers.go
@@ -0,0 +1,28 @@
+package helpers
+
+import (
+	"fmt"
+	"net/http"
+	"runtime/debug"
+
+	"github.com/arkadiuszekprogramista/bookingapp/internal/config"
+)
+
+var app *config.AppConfig
+
+
+// NewHelpers sets up app config for helpers
+func NewHelpers(a *config.AppConfig) {
+	app = a
+}
+
+func ClientError(w http.ResponseWriter, status int) {
+	app.InfoLog.Println("Client error whith status of", status)
+	http.Error(w, http.StatusText(status), status)
+}
+
+func ServerError(w http.ResponseWriter, err error) {
+	trace := fmt.Sprintf("%s\n%s", err.Error(), debug.Stack())
+	app.ErrorLog.Panicln(trace)
+	http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+}
