a905b552cc27fba8a5a37adbd75481aa8e65d87a
arkadiuszekprogramista <arkadiuszlaskowski1987@gmail.com>
2022-09-20 07:48:30 +0200

    Added POST tests for handlers

--- internal/handlers/handlers_test.go
+++ internal/handlers/handlers_test.go
@@ -3,6 +3,7 @@ package handlers
 import (
 	"net/http"
 	"net/http/httptest"
+	"net/url"
 	"testing"
 )
 
@@ -28,7 +29,20 @@ var theTest = []struct {
 	{"contact", "/contact", "GET",[]postData{}, http.StatusOK},
 	{"reservation-summary","/reservation-summary","GET",[]postData{}, http.StatusOK},
 	{"make-reservation","/make-reservation","GET",[]postData{}, http.StatusOK},
-
+	{"post-search","/search-availability","POST",[]postData{
+		{key: "start", value: "2020-01-01"},
+		{key: "end", value: "2020-01-12"},
+	}, http.StatusOK},
+	{"post-search-json","/search-availability-json","POST",[]postData{
+		{key: "start", value: "2020-01-01"},
+		{key: "end", value: "2020-01-12"},
+	}, http.StatusOK},
+	{"post-make-reservation","/make-reservation","POST",[]postData{
+		{key: "first_name", value: "Arka"},
+		{key: "last_name", value: "Las"},
+		{key: "email", value: "email@email.com"},
+		{key: "phone", value: "123-123-123"},
+	},http.StatusOK},
 
 }
 
@@ -48,10 +62,20 @@ func TestHandlers(t *testing.T) {
 			if resp.StatusCode != e.expectedStatusCode {
 				t.Errorf("for %s, expected %d but got %d", e.name, e.expectedStatusCode, resp.StatusCode)
 			}
-
-
 		} else {
+			values := url.Values{}
+			for _, param := range e.params{
+				values.Add(param.key, param.value)
+			}
+			resp, err := ts.Client().PostForm(ts.URL + e.url, values)
+			if err != nil {
+				t.Log(err)
+				t.Fatal(err)
+			}
 
+			if resp.StatusCode != e.expectedStatusCode {
+				t.Errorf("for %s, expected %d but got %d", e.name, e.expectedStatusCode, resp.StatusCode)
+			}
 		}
 	}
 }
